196edab38afe40e92ad488f10c6b9e1b385f4cc2
legendryflyer <tavish.anade@gmail.com>
2024-03-19 15:39:16 +0530

    added

---
+++ rev day6.py
@@ -0,0 +1,13 @@
+class calci:
+    def add(self,x,y):
+        return(x+y)
+    
+    def add(self,x,y,z):
+        return (x+y+z)
+    
+    def add(self,x,y,z,a):
+        return (x+y+z+a)
+    
+
+h=calci()
+h.add()
