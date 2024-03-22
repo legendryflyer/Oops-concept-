0e6fca026f1e1d48adb2380b953397bb9e8cb3b2
legendryflyer <tavish.anade@gmail.com>
2024-03-22 23:55:56 +0530

    added

---
+++ rev day7.py
@@ -0,0 +1,52 @@
+# ducktyping
+
+class duck:
+    def talk(self):
+        print("quack quack")
+        
+class human:
+    def talk(self):
+        print("hi hellow")
+        
+class cat:
+    def mew(self):
+        print("mewwww")
+        
+
+def callTalk(obj):
+    if hasattr(obj, "talk"):
+        obj.talk()
+    else:
+        obj.mew()
+        
+a=duck()
+b=human()
+c=cat()
+
+callTalk(a)
+callTalk(b)
+callTalk(c)
+
+# oprator overloding 
+
+print("i"+"a") # string 
+print(5+5) # integer 
+
+
+
+class bookA:
+    def __init__(self,pg):
+        self.pages=pg
+        
+class bookB:
+    def __init__(self,pg):
+        self.pages=pg
+
+
+    def __add__(self,other):
+        return bookA(self.pages + other.pages)
+
+book1 = bookA(100)
+book2 = bookB(80)
+print(book1.pages + book2.pages)
+print(book2.pages + book1.pages)
