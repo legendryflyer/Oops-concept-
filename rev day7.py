944456a75d7850f7ea83030d4c0fe53cdc3be1ec
legendryflyer <tavish.anade@gmail.com>
2024-03-23 00:02:59 +0530

    added

--- rev day7.py
+++ rev day7.py
@@ -45,8 +45,37 @@ class bookB:
 
     def __add__(self,other):
         return bookA(self.pages + other.pages)
+    
+    def __gt__(self,other):
+        return bookA(self.pages > other.pages)
 
 book1 = bookA(100)
 book2 = bookB(80)
 print(book1.pages + book2.pages)
 print(book2.pages + book1.pages)
+
+print(book1.pages>book2.pages)
+print(book2.pages>book1.pages)
+
+
+# overriding 
+
+class Animal:
+    def talk(self):
+        print("I am an animal.")
+        
+class Cat(Animal):
+    def talk(self):
+        print("Meow!")
+        
+class Dog(Animal):
+    def talk(self):
+        print("Woof!")
+        
+        
+d=Dog()
+e=Cat()
+
+d.talk()
+e.talk()
+        
